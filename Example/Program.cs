21439f8f0027d76c85642d930672975dfe6a14f9
EmmaBjerregard <emma701c@mail.hansenberg.dk>
2023-12-05 12:47:11 +0100

    Created c# projects
    Missing bool

---
+++ Example/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//I am a comment
+
+/*
+I
+Am
+A
+Comment
+*/
+
+
+
+// Console.Write("Line number one");
+
+// Console.Write("Line number two");
+
+// Console.WriteLine("Line number three");
+
+// Console.Write("Line number four");
+
+
+//Datatypes: int, float, string, bool
+/*
+int = helt tal (-5, 4, 6, 576 osv.)
+float = decimaltal (5.6, 4.5, 6.7 osv.)
+*/
+
+
+//Variables
+int x; //declaration x
+x = 4; //instantiation x
+
+int y = 5; //declaration and instantiation y
+
+Console.WriteLine("x =" + x + " and y =" + y);
+
